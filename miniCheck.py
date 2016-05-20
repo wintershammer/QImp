8d09999c15d7b03a59f099bd0c6d7202e89bf502
wintershammer <wintershammer@gmail.com>
2016-05-20 20:26:15 +0300

    Added rudimentary typechecking to QImp

---
+++ miniCheck.py
@@ -0,0 +1,332 @@
+import operator as op
+import math, quantumLib, oracleLib, functools
+import numpy as np
+import re
+import scipy.linalg as scipyAlg
+import typecheck
+from parsimonious.grammar import Grammar
+
+typeEnv =  {"tensor" : typecheck.envTensor, "measure": typecheck.envMeasure, "apply" : typecheck.envApply}
+
+class QImp(object):
+
+    def __init__(self, env={}):
+        self.env = env
+        defaultEnf(env)
+
+    def parse(self, source):
+        grammar = self.grammarFromDocStr()
+        return Grammar(grammar)['program'].parse(source)
+
+    def eval(self, source):
+        node = self.parse(source) if isinstance(source, str) else source
+        method = getattr(self, node.expr_name, lambda node, children: children)
+        if node.expr_name in ['ifelse', 'func']:
+            return method(node)
+        return method(node, [self.eval(n) for n in node])
+
+    def grammarFromDocStr(self):
+        #concat docstrings to generate grammar. careful: only visitor methods must have docstrings, and these must be grammar rules
+        grammar = '\n'.join(v.__doc__ for k, v in vars(self.__class__).items()
+                          if '__' not in k and hasattr(v, '__doc__') and v.__doc__)
+        return grammar
+        
+    def program(self, node, children):
+        'program = expr*'
+        return children
+
+    def expr(self, node, children):
+        'expr = _ (load / func / ifelse / call / comp / infixCall / prefixCall/  lista / assignment / boolLit / stringLit / complexLit / floatLit / intLit / name) _'
+        return children[1][0]
+
+    def func (self, node):
+        'func = "lambda" "(" lvalue ((sep lvalue)*)? ")" ":" lvalue ((sep lvalue)*)? "{" expr* "}" ( "(" expr* ((sep expr)*)? ")" )?'
+        _, _, param1, params, _, _, type1, types, _, expr, _ , app = node
+        param1 = self.eval(param1)
+        type1 = self.eval(type1)
+        paramRest = list(map(self.eval, params))
+        typeRest = list(map(self.eval,types))
+        listOfParams = []
+        listOfTypes = []
+        listOfParams.append(param1)
+        listOfTypes.append(type1)
+
+        
+        for item in paramRest[0]:
+            listOfParams.append(item[1])
+            
+        for item in typeRest[0]:
+            listOfTypes.append(item[1])
+
+        if (app.text): 
+            arg = self.eval(app)
+            arguments = [arg[0][1][0]]
+            for item in arg[0][2][0]:
+               arguments.append(item[1])
+            return(func(*arguments))
+
+        firstType = "null" #Temporary checks! I should write a parser to parse type signatures instead of manually encoding them here
+        if listOfTypes[0] == "qubit":
+            firstType = typecheck.Qubit
+        elif listOfTypes[0] == "nat":
+            firstType = typecheck.Nat
+        elif listOfTypes[0] == "qq":
+            firstType = typecheck.Lollipop(typecheck.Qubit,typecheck.Qubit)
+        elif listOfTypes[0] == "qn":
+            firstType = typecheck.Lollipop(typecheck.Qubit,typecheck.Qubit)
+        elif listOfTypes[0] == "qxq":
+            firstType = typecheck.Multiplicative(typecheck.Qubit,typecheck.Qubit)
+
+ 
+        topLam = typecheck.Lam(typecheck.Identifier(listOfParams[0]),firstType,[])
+        latestLam = topLam
+
+        typeEnv[listOfParams[0]] = firstType
+        
+        for item,typpe in list(zip(listOfParams,listOfTypes))[1:]:
+            typos = "null"
+            if typpe == "qubit":
+                typos = typecheck.Qubit
+            elif typpe == "nat":
+                typos = typecheck.Nat
+            elif typpe == "qq":
+                typos = typecheck.Lollipop(typecheck.Qubit,typecheck.Qubit)
+            typeEnv[item] = typos
+            currentLam = typecheck.Lam(typecheck.Identifier(item),typos,[])
+            latestLam.body = currentLam
+            latestLam = currentLam
+
+        
+        bodyExprs = []
+        body = []
+
+        for item in self.eval(expr):
+            if item != "IGNORE":
+                bodyExprs.append(item)
+
+        if len(bodyExprs) > 1:
+            body = functools.reduce(lambda x,y: typecheck.App(y,x),reversed(bodyExprs))
+        else:
+            body = bodyExprs[0]
+        
+        latestLam.body = body
+        
+        #typos = typecheck.typecheck(topLam,typeEnv)
+        #print(typos)
+        
+        return topLam
+
+    def lista(self, node, children):
+        'lista = "[" expr* "]"'
+        _, arguments, _ = children
+        return typecheck.Const(str(arguments),typecheck.Qubit)
+
+    def parameters(self, node, children):
+        'parameters = lvalue*'
+        return children
+
+    def ifelse(self, node):
+        'ifelse = "if" _ "(" expr ")" "{" expr* "}" _ "else" _ "{" expr* "}" '
+        _, _, _, cond, _, _, cons, _ , _ , _ , _ , _, alt, _ = node
+
+        consType = self.eval(cons)[0] 
+        altType = self.eval(alt)[0]
+        
+        if consType.typos != altType.typos or (isinstance(consType,typecheck.Const) and consType.typos != altType.typos) :
+            raise Exception("Consequent and alternative types don't match")
+            
+        return consType
+
+    def call(self, node, children): #na valo kommata anamesa! how to? opos to kana sthn valentine! (argument1, *(, argumentsExtra)* <- optional)
+        'call = name "(" expr ((sep expr)*)? ")"'
+        name, _, argument1, arguments, _= children
+
+        returner = []
+        returner.append(name)
+        returner.append(argument1)
+        
+        
+        for item in arguments[0]:
+            returner.append(item[1])
+
+        
+        expr = functools.reduce(lambda x,y: typecheck.App(x,y),returner)
+        
+        return expr
+        
+
+    def infixCall(self, node, children): #calling binary operators in infix style: (x f y)
+        'infixCall = "(" expr name expr ")"'
+        _, argument1, name, argument2, _= children
+        returner = []
+        returner.append(argument1)
+        returner.append(argument2)
+        return name(*returner)
+
+    def prefixCall(self, node, children): #calling unary operators prefix style: (f x) -- potentially could allow any arity, but i don't see the use for lisp-style function calls
+        'prefixCall = "(" name expr ")"'
+        _, name, argument, _= children
+        return name(argument)
+
+    def assignment(self, node, children): 
+        'assignment = "let" _ lvalue "=" expr'
+        _,_,lvalue, _, expr = children
+        if lvalue in self.env:
+            raise Exception("Duplicate definitions for" + ": " + lvalue)
+        typeEnv[lvalue] = typecheck.typecheck(expr,typeEnv)
+        return expr
+
+    def lvalue(self, node, children): #make that 'lvalue = ~"[a-z0-9]+" _' if you want variable/func names to have alphanumeric instead
+        'lvalue = ~"[a-zA-Z?]+" _'
+        return node.text.strip()
+
+    def boolLit(self, node, children):
+        'boolLit = _("#t" / "#f")_'
+        if (node.text == "#t"):
+            return True
+        else:
+            return False
+        return typecheck.Const(node.text,typecheck.Bool)
+        
+    def comp(self, node, children): 
+        'comp = name (compsep name)+ _ "(" expr* ")"'
+        nameO, nameR, _ , _, expr , _ = children   
+        for index,fun in enumerate(reversed(nameR)): #remember: the first function to be applied in a composition is the innermost one (thats why we reverse the list)
+            if (index == 0): #initialise: apply the innermost function to the expression
+                value = fun[1](*expr) 
+            else: 
+                value = fun[1](value) #apply the next fun in the queue
+        return nameO(value) #apply the outermost func and return
+
+
+    def load(self, node, children):
+        'load = "--load" _ lvalue '
+        _,_,filename = children
+        temp = QImp()
+        with open ( (filename + ".qimp" ), "r",encoding="utf8") as myfile:
+            temp.eval(myfile.read())
+            self.env = temp.env.copy()
+        
+    def stringLit(self, node, children):
+        'stringLit = "\\"" ~"[a-z A-Z 0-9 ! # $ ?]*" "\\"" '
+        return typecheck.Const(node.text,typecheck.String)
+    
+    def name(self, node, children): #make that 'name = ~"[a-z0-9]+" _' if you want variable/func names to have alphanumeric instead
+        'name = ~"[a-zA-Z⊗·+\-/*=?]+"'
+        return typecheck.Identifier(node.text)
+
+    def numeral(self, node, children): #helper parser, matches numeral literals
+        'numeral = ~"[0-9]+"'
+        
+    def intLit(self, node, children):
+        'intLit = ("-")? numeral'
+        return typecheck.Const(node.text,typecheck.Int)
+    
+    def floatLit(self,node,children):
+        'floatLit = ("-")? numeral "." numeral '
+        return typecheck.Const(node.text,typecheck.Float)
+
+    def complexLit(self, node, children): #bit of a mess: complexLit = optional "-", (float or integer), "+" or "-", (float or integer)
+        'complexLit = ("+"/"-")? (numeral ("." numeral)?) ("+"/"-") (numeral ("." numeral)?) "i"'
+        return typecheck.Const(node.text,typecheck.Complex)
+
+
+    def _(self, node, children):
+        '_ = ~"\s*"'
+
+    def sep(self,node,children):
+        'sep = _ "," _ '
+
+    def compsep(self,node,children):
+        'compsep = _"."_'
+
+
+class Function(object):
+    def __init__(self,env,parameters,body):
+        self.env = env
+        self.parameters = parameters
+        self.body = body
+
+
+    def __call__(self,*args):
+        
+        if(len(args) < len(self.parameters)):
+            env1 = dict(list(self.env.items()) + list(zip(self.parameters[:len(args)], args)))
+            return Function(env1,self.parameters[len(args):],self.body)
+        env1 = dict(list(self.env.items()) + list(zip(self.parameters, args)))
+        return QImp(env1).eval(self.body)[-1]    
+        
+        
+
+def myCar(l):
+    if isinstance(l,list):
+        return l[0]
+    else:
+        return []
+    
+def myCdr(l):
+    if isinstance(l,list):
+        return l[1:]
+    else:
+        return []
+
+def prettyPrint(x):
+    if (isinstance(x,list)): #if our item to print is a list, chcek to see if its a list of lists
+        if any(isinstance(el, list) for el in x): #if its a list of lists print it like a matrix
+            print("---Matrix---")
+            for item in x:
+                print(item)
+    else:
+        print(x)
+
+
+def defaultEnf(env):
+    env['sum'] = lambda *args: sum(args)
+    env['sqrt'] = lambda x: math.sqrt(x)
+    env['print'] = lambda *x: print(*x)
+    env['pprint'] = lambda *x: prettyPrint(*x)
+    env['car'] = lambda x: myCar(x)
+    env['cdr'] = lambda x: myCdr(x)
+    env['map'] = lambda x,y: list(map(x,y))
+    env['fold'] = lambda x,y : functools.reduce(x,y)
+    env['tensor'] = lambda x,y: np.kron(x,y)
+    env['apply'] = lambda x,y: np.dot(x,y)
+    env['outer'] = lambda x,y: np.outer(x,y)
+    env['measure'] = lambda x: quantumLib.measure(x)
+    env['subsystems'] = lambda state,configuration: quantumLib.splitToSub(state,configuration)
+    env['eq'] = lambda x,y: x == y
+    env['append'] = lambda x,y : [x]+y
+    env['prepend'] = lambda x,y : y + [x]
+    env['⊗'] = lambda x,y: np.kron(x,y)
+    env['·'] = lambda x,y: np.dot(x,y) 
+    env["-"] = lambda x,y: np.subtract(x,y) #numpy add/sub/mult/div works like regular for matrix AND numbers! no need for addMatrix etc
+    env["+"] = lambda x,y: np.add(x,y)
+    env["*"] = lambda x,y: np.multiply(x,y)
+    env["/"] = lambda x,y: np.divide(x,y)
+    env["="] = lambda x,y: x == y
+    env["len"] = lambda x: len(x)
+    env["null?"] = lambda x: len(x) == 0
+    env["sqrt"] = lambda x: np.sqrt(x)
+    env["reverse"] = lambda x : list(reversed(x))
+    env["fold"] = lambda x,y : functools.reduce(x,y)
+    env["pi"] = math.pi
+    env["exp"] = np.exp #should add cleanExp like quantum parethesis
+    env["oracle"] = lambda fun: oracleLib.generateOracle(fun)
+    env["expm"] = lambda matrix: list(scipyAlg.expm(matrix))
+    env["logm"] = lambda matrix: list(scipyAlg.logm(matrix))
+    env["logTwo"] = lambda x: int(math.log(x,2))
+    env["length"] = lambda x: len(x)
+    env["transpose"] = lambda x: (quantumLib.ctransp(x)).tolist();
+
+def repl():
+    qImpInstance = QImp()
+    while True:
+        print(qImpInstance.eval(input(">>>")))
+
+with open ("test.qimp", "r",encoding="utf8") as myfile:
+    a = QImp()
+    kek  = a.eval(myfile.read())
+    for item in kek:
+        print(typecheck.typecheck(item,typeEnv))
+    
+    #print("Global env:",a.env)
--- typecheck.py
+++ typecheck.py
@@ -2,11 +2,24 @@
 
 # ast "nodes"
 
+
+class Const:
+
+    def __init__(self,expr,typos):
+        self.expr = expr
+        self.typos = typos
+
+    def __str__(self):
+        return self.expr
+    
+    
 class Identifier: 
 
     def __init__(self,name):
         self.name = name
 
+    def __str__(self):
+        return self.name
 
 class Lam:
     def __init__(self,var,typ,body):
@@ -14,21 +27,45 @@ class Lam:
         self.typ = typ
         self.body = body
 
+    def __str__(self):
+        return "lam {0} {1} {2}".format(str(self.var),str(self.typ),str(self.body))
+
 class App:
 
     def __init__(self,e1,e2): #e1 e2 (application)
         self.e1 = e1
         self.e2 = e2
+
+    def __str__(self):
+        return "app ({0} {1})".format((self.e1),str(self.e2))
         
 class Tensor:
 
     def __init__(self,e1,e2): #e1 e2 (application)
         self.e1 = e1
         self.e2 = e2
+
+    def __str__(self):
+        return "{0} (x) {1}".format(str(self.e1),str(self.e2))
+    
+        
 #types
 
 Qubit = "Qubit"
-Nat = "Nat"
+Int = "Int"
+Complex = "Complex"
+Float = "Float"
+Bool = "Bool"
+String = "String"
+
+class Exponential:
+
+    def __init__(self,typ):
+        self.typ = typ
+
+    def __str__(self):
+        return "!{0}".format(self.typ)
+
 
 class Lollipop:
 
@@ -39,14 +76,16 @@ class Lollipop:
     def __str__(self):
         return "{0} -<> {1}".format(str(self.t1),str(self.t2))
 
-class Exponential:
+class Multiplicative:
 
     def __init__(self,t1,t2): # t1 -<> t2
         self.t1 = t1
         self.t2 = t2
 
     def __str__(self):
-        return "{0} (x) {1}".format(str(self.t1),str(self.t2))    
+        return "{0} (x) {1}".format(str(self.t1),str(self.t2))
+
+
 # typechecking
 
 def getType(name,env):
@@ -60,9 +99,16 @@ def typecheck(item,env):
     
     if isinstance(item,Identifier):
         varType = getType(item.name,env)
-        del env[item.name]
+        if not (isinstance(varType,Exponential)):
+            del env[item.name]
+        else:
+            varType = varType.typ
         return varType
 
+    
+    elif isinstance(item,Const):
+        return item.typos
+    
     elif isinstance(item,Lam):
         env[item.var.name] = item.typ
         bodyType = typecheck(item.body,env)
@@ -76,23 +122,34 @@ def typecheck(item,env):
             if(lamType.t1 == argType):
                 return lamType.t2
             else:
-                raise Exception("Function expecting type {0} but was given {1}".format(lamType.t1,argType))
+                #check subtyping
+                
+                if(lamType.t1 == Qubit and isinstance(argType,Multiplicative) or isinstance(argType,Exponential)):
+                    return lamType.t2
+                else:
+                    raise Exception("Function expecting type {0} but was given {1}".format(lamType.t1,argType))
         else:
                 raise Exception("Non-function application")
             
     elif isinstance(item,Tensor):
         lType = typecheck(item.e1,env)
         rType = typecheck(item.e2,env)
-        return Exponential(lType,rType)
+        return Multiplicative(lType,rType)
         
 
 def assertBindingUsed(name,env):
     if name in env:
-        raise Exception("Binding {0} not used".format(name))
+        if not isinstance(env[name],Multiplicative):
+            raise Exception("Binding {0} not used".format(name))
 
+
+envApply = Exponential(Lollipop(Lollipop(Qubit,Qubit),Qubit))
+envQIf = Exponential(Lollipop(Exponential(Bool),Exponential(Qubit))) #should I make one if for each type?
+envTensor = Exponential(Lollipop(Qubit,Lollipop(Qubit,Multiplicative(Qubit,Qubit))))
+envMeasure = Exponential(Lollipop(Qubit,Exponential(Qubit)))
 #tests
-env = {}
-expr = Lam(Identifier("x"),Qubit,Lam(Identifier("y"),Qubit,Tensor(Identifier("x"),Identifier("y"))))
-lol = typecheck(expr,env)
-print(lol)
+#env = {}
+##expr = Lam(Identifier("x"),Lollipop(Qubit,Qubit),Lam(Identifier("y"),Lollipop(Qubit,Qubit),Lam(Identifier("z"),Qubit,App(Identifier("x"),App(Identifier("y"),Identifier("z"))))))
+#lol = typecheck(expr,env)
+#print(lol)
 
